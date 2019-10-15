26bfcb79341c4ff7cc35f65a677f55dead51a10d
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2019-10-15 14:57:24 +0800

    string balance complete commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StringBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "RLRRLLRLRL";
+            var result = BalancedStringSplit(s);
+            Console.WriteLine(result);
+        }
+
+        public static int BalancedStringSplit(string s)
+        {
+            int count = 0;
+            int numberCount = 0;
+            foreach(var i in s)
+            {
+                if(i == 'R')
+                {
+                    numberCount++;
+                }
+                else
+                {
+                    numberCount--;
+                }
+                if(numberCount == 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
