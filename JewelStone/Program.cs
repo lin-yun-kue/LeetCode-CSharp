de1a2cc0bb687d4bf422f87bdf7123da5841718b
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2019-10-15 17:40:13 +0800

    JewelStone complete

---
+++ JewelStone/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace JewelStone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jewel = "aA";
+            var stones = "aAAbbb";
+            var result = NubJewelsInStones(jewel, stones);
+            Console.WriteLine(result);
+        }
+
+        public static int NubJewelsInStones(string J, string S)
+        {
+            //method one
+            //return S.Count(x => J.Contains(x));
+
+            //method two
+            var count = 0;
+            S.ToList().ForEach(x => {
+                if (J.Contains(x))
+                {
+                    count++;
+                }
+            });
+            return count;
+        }
+    }
+}
