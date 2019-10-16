9d38c3b88f2b59abe49c57033e1d14b78543486f
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2019-10-16 17:34:33 +0800

    add description

--- RangeSumBST/Program.cs
+++ RangeSumBST/Program.cs
@@ -2,6 +2,14 @@
 
 namespace RangeSumBST
 {
+    /// <summary>
+    /// Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+    /// The binary search tree is guaranteed to have unique values.
+    /// 
+    /// Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+    /// Output: 32
+    /// 
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
--- StringBalance/Program.cs
+++ StringBalance/Program.cs
@@ -4,6 +4,17 @@ namespace StringBalance
 {
     class Program
     {
+        /// <summary>
+        /// Balanced strings are those who have equal quantity of 'L' and 'R' characters.
+        /// Given a balanced string s split it in the maximum amount of balanced strings.
+        /// Return the maximum amount of splitted balanced strings.
+        /// 
+        /// Input: s = "RLRRLLRLRL"
+        /// Output: 4
+        /// Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             var s = "RLRRLLRLRL";
@@ -15,9 +26,9 @@ namespace StringBalance
         {
             int count = 0;
             int numberCount = 0;
-            foreach(var i in s)
+            foreach (var i in s)
             {
-                if(i == 'R')
+                if (i == 'R')
                 {
                     numberCount++;
                 }
@@ -25,7 +36,7 @@ namespace StringBalance
                 {
                     numberCount--;
                 }
-                if(numberCount == 0)
+                if (numberCount == 0)
                 {
                     count++;
                 }
