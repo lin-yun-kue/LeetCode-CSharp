8dd5d1980cc9006ee7f83b43df5f5b117f51d375
GIT4U\Kyle.Lin <kyle.lin@git4u.com>
2019-12-04 17:10:32 +0800

    Divisor_Game complete

---
+++ Divisor Game/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Divisor_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = 5;
+            var solution = new Solution();
+            var result = solution.DivisorGame(input);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Solution
+    {
+        public bool DivisorGame(int N)
+        {
+            var aRound = true;
+            while(N != 1)
+            {
+                N -= 1;
+                aRound = !aRound;
+            }
+            return aRound == true ? false : true;
+        }
+
+        public bool DivisorGame1(int N)
+        {
+            return N % 2 == 0;
+        }
+
+
+    }
+}
